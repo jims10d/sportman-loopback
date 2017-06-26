27d9bf1fa80e4e871203b7365a08ec00bae01117
Jimmy Chen <jimmy600d@gmail.com>
2017-06-26 19:24:24 +0700

    tes

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var app = module.exports = loopback();
 
 app.start = function() {
   // start the web server
+  app.models.user.settings.acls = require('./user.json');
   return app.listen(function() {
     app.emit('started');
     var baseUrl = app.get('url').replace(/\/$/, '');
