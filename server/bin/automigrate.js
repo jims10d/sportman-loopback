8729e1699d451828494acde8e1bdfe347246181d
Jimmy Chen <jimmy600d@gmail.com>
2017-06-27 09:58:47 +0700

    acl

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,37 +1,20 @@
 var app = require('../server');
  
-var teams = [
-  {
-    team_abrName: 'mu',
-    team_name: 'manchester united',
-    team_logo: '',
-    team_coach: '',
-    team_position: '',
-    team_play: '',
-    team_point: '',
-    team_win: '',
-    team_draw: '',
-    team_lose: '',
-    team_goalFor: '',
-    team_goalAgainst: '',
-    coach_id: '',
-    team_squad: []
-  }
-];
+var ACLs = [];
  
 // this loads the accountDb configuration in ~/server/datasources.json
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.automigrate('team', function(err) {
+dataSource.automigrate('ACL', function(err) {
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Team = app.models.Team;
-  var count = teams.length;
-  teams.forEach(function(team) {
+  var ACL = app.models.ACL;
+  var count = ACLs.length;
+  ACLs.forEach(function(ACL) {
      // insert new records into the Account table
-     Team.create(team, function(err, record) {
+     ACL.create(ACL, function(err, record) {
       if (err) return console.log(err);
  
       console.log('Record created:', record);
