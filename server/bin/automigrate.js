05710e2c2c24ce3d95075dbd9e090b4c29c61433
Jimmy Chen <jimmy600d@gmail.com>
2017-06-28 15:51:32 +0700

    add teamInvitation to user model

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,22 +1,14 @@
 var app = require('../server');
 
-var teams = [ // kecils
+var users = [ // kecils
   {
-    team_abrName: 'BBFC',
-    team_name: 'Black Bull FC',
-    team_logo: 'string',
-    team_coach: 'Blacker',
-    team_position: 2,
-    team_play: 1,
-    team_point: 3,
-    team_win: 1,
-    team_draw: 0,
-    team_lose: 0,
-    team_goalFor: 4,
-    team_goalAgainst: 0,
-    team_coach_id: 'T010',
-    invited_member: 'ozil11',
-    team_squad: []
+    id: 'U015',
+    username: 'uso',
+    fullname: 'usolando',
+    password: 'uso',
+    email: 'uso@gmail.com',
+    role: 'player',
+    teamInvitation: 'blacker'
   }
 ];
  
@@ -24,15 +16,15 @@ var teams = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.automigrate('team', function(err) { // kecil
+dataSource.autoupdate('user', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Team = app.models.Team; // besar
-  var count = teams.length; // kecils
-  teams.forEach(function(team) { // kecils
+  var User = app.models.User; // besar
+  var count = users.length; // kecils
+  users.forEach(function(user) { // kecils kecil
      // insert new records into the Account table
-     Team.create(team, function(err, record) { // besar kecil
+     User.create(user, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
