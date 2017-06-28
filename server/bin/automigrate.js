43408f0191dcc7ebbf5f35603c443e3d272aac00
Jimmy Chen <jimmy600d@gmail.com>
2017-06-28 15:35:55 +0700

    automigrate team

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,20 +1,38 @@
 var app = require('../server');
- 
-var ACLs = [];
+
+var teams = [ // kecils
+  {
+    team_abrName: 'BBFC',
+    team_name: 'Black Bull FC',
+    team_logo: 'string',
+    team_coach: 'Blacker',
+    team_position: 2,
+    team_play: 1,
+    team_point: 3,
+    team_win: 1,
+    team_draw: 0,
+    team_lose: 0,
+    team_goalFor: 4,
+    team_goalAgainst: 0,
+    team_coach_id: 'T010',
+    invited_member: 'ozil11',
+    team_squad: []
+  }
+];
  
 // this loads the accountDb configuration in ~/server/datasources.json
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.automigrate('ACL', function(err) {
+dataSource.automigrate('team', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var ACL = app.models.ACL;
-  var count = ACLs.length;
-  ACLs.forEach(function(ACL) {
+  var Team = app.models.Team; // besar
+  var count = teams.length; // kecils
+  teams.forEach(function(team) { // kecils
      // insert new records into the Account table
-     ACL.create(ACL, function(err, record) {
+     Team.create(team, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
