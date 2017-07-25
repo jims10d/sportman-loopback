6ae5afafc7bedff7a91c9b9e29a1f81eb4b0352a
Jimmy Chen <jimmy600d@gmail.com>
2017-07-25 21:28:50 +0700

    add delete function

--- common/models/match.js
+++ common/models/match.js
@@ -1,3 +1,24 @@
 module.exports = function(Match) {
+	Match.delMatch = function(fixNumber, cb){
+		Match.destroyAll({where:{fixture_number:fixNumber}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
+	};
 
+	Match.remoteMethod(
+		'delMatch',
+		{
+			accepts: {arg: 'fixNumber', type: 'string'},
+			returns: {arg: 'id', type: 'string', root: true},
+			http: {path: '/delMatch', verb: 'delete', source: 'query'},
+			description: "Get Team instance by team name"
+		}
+	);
 };
+
+
