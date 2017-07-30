40160f12a6a515b6a73f79951754a8fda1b286fa
Jimmy Chen <jimmy600d@gmail.com>
2017-07-30 13:45:09 +0700

    update model message

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,18 +1,17 @@
 var app = require('../server');
 
-var matches = [ // kecils
+var messages = [ // kecils
   {
-    match_name: 'SFC',
-    match_venue: 'Spirit FC',
-    match_time: '08:00',
-    match_date: '29 Juli 2017',
-    match_referee: 'sdfsfs',
-    match_homeTeam: 'qwerty',
-    match_awayTeam: 'asdfg',
-    match_fixture: '10',
-    match_started: 'false',
-    match_homeScore: '2',
-    match_awayScore: '1',
+    date: '29 Juli 2017',
+    sender: 'jimmy',
+    receiver: 'luffy',
+    sender_name: 'jimmy',
+    receiver_name: 'luffy',
+    id: 'm007',
+    content: 'hello luffy',
+    read: '',
+    readStatus: 'false',
+    photo: ''
   }
 ];
  
@@ -20,15 +19,15 @@ var matches = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('match', function(err) { // kecil
+dataSource.autoupdate('message', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Match = app.models.Match; // besar
-  var count = matches.length; // kecils
-  matches.forEach(function(match) { // kecils kecil
+  var Message = app.models.Message; // besar
+  var count = messages.length; // kecils
+  messages.forEach(function(message) { // kecils kecil
      // insert new records into the Account table
-     Match.create(match, function(err, record) { // besar kecil
+     Message.create(message, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
