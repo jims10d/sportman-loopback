4eb5c8df15f3f8f48835881b14f10bd374da959c
Jimmy Chen <jimmy600d@gmail.com>
2017-08-20 09:37:01 +0700

    edit competition model

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,71 +1,20 @@
 var app = require('../server');
 
-var matches = [ // kecils
+var competitions = [ // kecils
   {
-    match_name: 'howay',
-    match_venue: 'sdfs fg',
-    match_time: '11:30',
-    match_countDown: '11:29',
-    match_length: '50',
-    match_break: '5',
-    match_date: '07/31/2017',
-    match_referee: 'luffy',
-    match_homeTeam: 'home',
-    match_awayTeam: 'away',
-    match_fixture: '10',
-    match_started: false,
-    timer_status: '',
-    match_homeScore: '0',
-    match_awayScore: '0',
-    timer: '0',
-    countDownTimer: '60',
-    countDownStarted: false,
-    halfTime: false,
-    fullTime: false,
-    goalHome: {
-      scorer: 'jims',
-      time: '7',
-      scoreHome: '0',
-      scoreAway: '0',
-    },
-    goalAway: {
-      scorer: 'jims',
-      time: '8'
-    },
-    assistHome: {
-      assister: 'jims',
-      time: '7'
-    },
-    assistAway: {
-      assister: 'jims',
-      time: '7'
-    },
-    yellowCardHome: {
-      player: 'jims',
-      time: '7'
-    },
-    yellowCardAway: {
-      player: 'jims',
-      time: '7'
-    },
-    redCardHome: {
-      player: 'jims',
-      time: '7'
-    },
-    redCardAway: {
-      player: 'jims',
-      time: '7'
-    },
-    substituteHome: {
-      in: 'jims',
-      out: 'zoro',
-      time: '7'
-    },
-    substituteAway: {
-      in: 'jims',
-      out: 'zoro',
-      time: '7'
-    }
+    id: 'C1210hdkfh',
+    organizer: 'Organizer',
+    comp_name: 'Sporto Cup',
+    comp_fee: '500.000',
+    comp_regulation: 'afsjdfhk h',
+    comp_type: 'fsfd',
+    comp_location: 'Tangkas Sport',
+    comp_finish: '08/20/2017',
+    comp_start: '08/25/2017',
+    registeredTeam: 'redcat',
+    comp_notes: 'asdad',
+    comp_numOfTeams: '16',
+    comp_award: '1 juta',
   }
 ];
  
@@ -73,15 +22,15 @@ var matches = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('match', function(err) { // kecil
+dataSource.autoupdate('competition', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Match = app.models.Match; // besar
-  var count = matches.length; // kecils
-  matches.forEach(function(match) { // kecils kecil
+  var Competition = app.models.Competition; // besar
+  var count = competitions.length; // kecils
+  competitions.forEach(function(competition) { // kecils kecil
      // insert new records into the Account table
-     Match.create(match, function(err, record) { // besar kecil
+     Competition.create(competition, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
