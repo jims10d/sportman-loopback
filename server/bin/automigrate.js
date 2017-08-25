1e9c244231e88ee51952e6a1f375c337feef2f81
Jimmy Chen <jimmy600d@gmail.com>
2017-08-25 17:51:52 +0700

    update model match

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,23 +1,38 @@
 var app = require('../server');
 
-var users = [ // kecils
+var matches = [ // kecils
   {
-    id: 'U1210hdkfhdds',
-    username: 'vinsmoke',
-    fullname: 'vinsmoke',
-    password: 'vinsmoke',
-    email: 'vinsmoke@sportman.com',
-    role: 'Player',
-    teamInvitation: '',
-    team: '',
-    teamRequested: '',
-    age: '20',
-    address: 'lebak bulus',
-    position: 'Striker,Defender',
-    hp: '089420312340',
-    bio: 'I am using sportman',
-    profileCompleted: 'false',
-
+    match_name: 'qwe',
+    match_venue: 'qwe',
+    match_time: 'vinsmoke',
+    match_countDown: 'vinsmoke',
+    match_length: 'vinsmoke@sportman.com',
+    match_break: 'Player',
+    match_date: 'qwe',
+    match_referee: 'qwe',
+    match_homeTeam: 'qwe',
+    match_awayTeam: '20',
+    match_fixture: 'lebak bulus',
+    match_started: 'Striker,Defender',
+    timer_status: '089420312340',
+    match_homeScore: 'I am using sportman',
+    match_awayScore: 'false',
+    schedule_status: 'U1210hdkfhdds',
+    timer: 'vinsmoke',
+    countDownTimer: 'vinsmoke',
+    countDownStarted: 'vinsmoke',
+    halftime: 'vinsmoke@sportman.com',
+    fulltime: 'Player',
+    goalHome: 'qwe',
+    goalAway: 'qwe',
+    assistHome: 'qwe',
+    assistAway: '20',
+    yellowCardHome: 'lebak bulus',
+    yellowCardAway: 'Striker,Defender',
+    redCardHome: '089420312340',
+    redCardAway: 'I am using sportman',
+    substituteHome: 'false',
+    substituteAway: 'U1210hdkfhdds'
   }
 ];
  
@@ -25,15 +40,15 @@ var users = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('user', function(err) { // kecil
+dataSource.autoupdate('match', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var User = app.models.User; // besar
-  var count = users.length; // kecils
-  users.forEach(function(user) { // kecils kecil
+  var Match = app.models.Match; // besar
+  var count = matches.length; // kecils
+  matches.forEach(function(match) { // kecils kecil
      // insert new records into the Account table
-     User.create(user, function(err, record) { // besar kecil
+     Match.create(match, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
