f67858651873298b8294534d2cc1af6c99798223
Jimmy Chen <jimmy600d@gmail.com>
2017-08-25 18:08:53 +0700

    update model match & competition

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,38 +1,20 @@
 var app = require('../server');
 
-var matches = [ // kecils
+var competitions = [ // kecils
   {
-    match_name: 'qwe',
-    match_venue: 'qwe',
-    match_time: 'vinsmoke',
-    match_countDown: 'vinsmoke',
-    match_length: 'vinsmoke@sportman.com',
-    match_break: 'Player',
-    match_date: 'qwe',
-    match_referee: 'qwe',
-    match_homeTeam: 'qwe',
-    match_awayTeam: '20',
-    match_fixture: 'lebak bulus',
-    match_started: 'Striker,Defender',
-    timer_status: '089420312340',
-    match_homeScore: 'I am using sportman',
-    match_awayScore: 'false',
-    schedule_status: 'U1210hdkfhdds',
-    timer: 'vinsmoke',
-    countDownTimer: 'vinsmoke',
-    countDownStarted: 'vinsmoke',
-    halftime: 'vinsmoke@sportman.com',
-    fulltime: 'Player',
-    goalHome: 'qwe',
-    goalAway: 'qwe',
-    assistHome: 'qwe',
-    assistAway: '20',
-    yellowCardHome: 'lebak bulus',
-    yellowCardAway: 'Striker,Defender',
-    redCardHome: '089420312340',
-    redCardAway: 'I am using sportman',
-    substituteHome: 'false',
-    substituteAway: 'U1210hdkfhdds'
+    id: 'qweasda',
+    organizer: 'qwe',
+    comp_name: 'ada',
+    comp_fee: '900',
+    comp_regulation: '',
+    comp_type: 'group stage',
+    comp_location: 'pontianak',
+    comp_finish: '27/08/2017',
+    comp_start: '25/08/2017',
+    registeredTeam: '',
+    comp_numOfTeam: '20',
+    comp_award: '1jt',
+    schedule_status: 'false'
   }
 ];
  
@@ -40,15 +22,15 @@ var matches = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('match', function(err) { // kecil
+dataSource.autoupdate('competition', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Match = app.models.Match; // besar
-  var count = matches.length; // kecils
-  matches.forEach(function(match) { // kecils kecil
+  var Competition = app.models.Competition; // besar
+  var count = competitions.length; // kecils
+  competitions.forEach(function(competition) { // kecils kecil
      // insert new records into the Account table
-     Match.create(match, function(err, record) { // besar kecil
+     Competition.create(competition, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
