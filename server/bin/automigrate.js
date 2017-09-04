280a323b67bd5e1b7338cebf9561edc8b478baf7
Jimmy Chen <jimmy600d@gmail.com>
2017-09-04 20:00:02 +0700

    update

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,40 +1,16 @@
 var app = require('../server');
 
-var matches = [ // kecils
+var classements = [ // kecils
   {
-    match_name: 'qqwe',
-    match_venue: 'qwe',
-    match_time: '12:00',
-    match_countDown: '',
-    match_length: '40',
-    match_break: '20',
-    match_date: '',
-    match_referee: '',
-    match_homeTeam: 'q',
-    match_awayTeam: 'w',
-    match_started: '',
-    match_fixture: '',
-    timer_status: '',
-    match_homeScore: '',
-    match_awayScore: '',
-    winner: '',
-    loser: '',
-    match_status: 'firstRound',
-    timer: '',
-    countDownTimer: '',
-    countDownStarted: '',
-    halfTime: '',
-    fullTime: '',
-    goalHome: '',
-    goalAway: '',
-    assistHome: '',
-    assistAway: '',
-    yellowCardHome: '',
-    yellowCardAway: '',
-    redCardHome: '',
-    redCardAway: '',
-    substituteHome: '',
-    substituteAway: '',
+    id: 'CLqqwe',
+    position: '12',
+    team: 'asdads',
+    play: '1',
+    win: '40',
+    draw: '20',
+    lose: '12',
+    goalDifference: '12',
+    points: '12'
   }
 ];
  
@@ -42,15 +18,15 @@ var matches = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('fixture', function(err) { // kecil
+dataSource.autoupdate('classement', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Match = app.models.Match; // besar
-  var count = matches.length; // kecils
-  matches.forEach(function(match) { // kecils kecil
+  var Classement = app.models.Classement; // besar
+  var count = classements.length; // kecils
+  classements.forEach(function(classement) { // kecils kecil
      // insert new records into the Account table
-     Match.create(match, function(err, record) { // besar kecil
+     Classement.create(classement, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
