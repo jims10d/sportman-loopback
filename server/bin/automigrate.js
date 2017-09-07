523c8a9b3cbfb6c4774dd1a5670a161ab7f02170
Jimmy Chen <jimmy600d@gmail.com>
2017-09-07 12:54:49 +0700

    update model match

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -1,15 +1,41 @@
 var app = require('../server');
 
-var trainings = [ // kecils
+var matches = [ // kecils
   {
-    id: 'Tqqsdsdwe',
-    coach: '12',
-    training_name: 'asdads',
-    training_location: 'gf',
-    training_date: '24 aug 2017',
-    training_time: '20:00',
-    acceptedPlayer: '',
-    deniedPlayer: ''
+    match_name: 'Tqqsdsdwe',
+    match_venue: 'asdasdasd',
+    match_time: '',
+    match_countDown: '',
+    match_length: '',
+    match_break: '',
+    match_date: '',
+    match_referee: '',
+    match_homeTeam: '',
+    match_awayTeam: '',
+    match_fixture: '',
+    match_number: '',
+    match_started: '',
+    timer_status: '',
+    match_homeScore: '',
+    match_awayScore: '',
+    winner: '',
+    loser: '',
+    match_status: '',
+    timer: '',
+    countDownTimer: '',
+    countDownStarted: '',
+    halfTime: '',
+    fullTime: '',
+    goalHome: '',
+    goalAway: '',
+    assistHome: '',
+    assistAway: '',
+    yellowCardHome: '',
+    yellowCardAway: '',
+    redCardHome: '',
+    redCardAway: '',
+    substituteHome: '',
+    substituteAway: ''
   }
 ];
  
@@ -17,15 +43,15 @@ var trainings = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('training', function(err) { // kecil
+dataSource.autoupdate('match', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Training = app.models.Training; // besar
-  var count = trainings.length; // kecils
-  trainings.forEach(function(training) { // kecils kecil
+  var Match = app.models.Match; // besar
+  var count = matches.length; // kecils
+  matches.forEach(function(match) { // kecils kecil
      // insert new records into the Account table
-     Training.create(training, function(err, record) { // besar kecil
+     Match.create(match, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
