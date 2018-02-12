0779c0bd4e746faebe509892ecf2db0c6277d139
Jimmy Chen <jimmy600d@gmail.com>
2018-02-12 13:15:53 +0700

    try json data type

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -16,7 +16,7 @@ var teams = [ // kecils
     team_goalFor: '50',
     team_goalAgainst: '0',
     coach_id: 'Cqweoqwoieu1',
-    invited_member: '[{"username":"jims", "date":"12/08/2018"}]',
+    invited_member: '{"username":"jims", "date":"12/08/2018"}',
     team_squad: 'a,b,c,d,e',
     player_request: '',
     coach_request: ''
