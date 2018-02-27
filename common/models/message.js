f0e1d6fbcfa1f3c7dd292a305b2069383bc864d9
Jimmy Chen <jimmy600d@gmail.com>
2018-02-27 12:33:41 +0700

    add function getChatData

--- common/models/message.js
+++ common/models/message.js
@@ -56,6 +56,46 @@ module.exports = function(Message) {
 			});
 	};
 
+
+	Message.getChatData = function(sender, receiver, cb){
+		Message.find({where:{sender: sender, receiver: receiver}}, // get all data except id from database
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					senderMessage = JSON.stringify(instance); // converts a JavaScript value to a JSON string
+					var parsed = JSON.parse(senderMessage);// parsing string
+
+					var allMessages = []; // array of string, this variabel combines senderMessage & receiverMessage
+
+					for(var x in parsed){
+					  allMessages.push(parsed[x]); // push senderMessage to allMessages
+					}
+
+					Message.find({where:{sender: receiver, receiver: sender}},
+						function(err,instance){
+							if(instance===null){
+								cb(null,null);
+							}else{
+								receiverMessage = JSON.stringify(instance); // converts a JavaScript value to a JSON string
+								var parsed2 = JSON.parse(receiverMessage);
+
+								for (var y in parsed2){
+									allMessages.push(parsed2[y]); // push receiverMessage to allMessages
+								}
+
+								allMessages.sort(function(a, b){ // sorting array by date (ascending)
+								    return a.msgOrder>b.msgOrder;
+								});
+								// console.log(allMessages);
+								cb(null,allMessages);
+							}
+					});
+				}
+			});
+	};
+
+
 	Message.addMessage = function(data, cb){
 		var User = Message.app.models.user;
 		Message.create(data,
@@ -285,6 +325,18 @@ module.exports = function(Message) {
 					],
 			returns: {arg: 'id', type: 'string', root: true},
 			http: {path: '/getMessage', verb: 'get', source: 'query'}
+		}
+	);
+
+	Message.remoteMethod(
+		'getChatData',
+		{
+			accepts : [
+						{arg : 'sender', type: 'string'},
+						{arg : 'receiver', type: 'string'}
+					],
+			returns: {arg: 'id', type: 'string', root: true},
+			http: {path: '/getChatData', verb: 'get', source: 'query'}
 		}
 	);
 
