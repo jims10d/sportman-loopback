d0abe56efd0b0308ccca4ba2bd78c65993c9b957
Jimmy Chen <jimmy600d@gmail.com>
2018-04-10 23:41:36 +0700

    update model message

--- common/models/message.js
+++ common/models/message.js
@@ -1,5 +1,8 @@
 module.exports = function(Message) {
+
 	Message.getMessage = function(sender, receiver, cb){
+		var allMessages = [];
+
 		Message.find({where:{sender: sender, receiver: receiver}}, // get all data except id from database
 			function(err,instance){
 				if(instance===null){
@@ -8,35 +11,31 @@ module.exports = function(Message) {
 					senderMessage = JSON.stringify(instance); // converts a JavaScript value to a JSON string
 					var parsed = JSON.parse(senderMessage);// parsing string
 
-					var allMessages = []; // array of string, this variabel combines senderMessage & receiverMessage
-
 					for(var x in parsed){
 					  allMessages.push(parsed[x]); // push senderMessage to allMessages
 					}
-
-					Message.find({where:{sender: receiver, receiver: sender}},
-						function(err,instance){
-							if(instance===null){
-								cb(null,null);
-							}else{
-								receiverMessage = JSON.stringify(instance); // converts a JavaScript value to a JSON string
-								var parsed2 = JSON.parse(receiverMessage);
-
-								for (var y in parsed2){
-									allMessages.push(parsed2[y]); // push receiverMessage to allMessages
-								}
-
-								allMessages.sort(function(a, b){ // sorting array by date (ascending)
-								    return a.date.getDate() - b.date.getDate();
-								});
-								console.log(allMessages);
-								// cb(null,allMessages);
-							}
-					});
-
-					cb(null,allMessages);
 				}
 			});
+
+		Message.find({where:{sender: receiver, receiver: sender}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					receiverMessage = JSON.stringify(instance); // converts a JavaScript value to a JSON string
+					var parsed2 = JSON.parse(receiverMessage);
+
+					for (var y in parsed2){
+						allMessages.push(parsed2[y]); // push receiverMessage to allMessages
+					}
+				}
+			});
+
+		allMessages.sort(function(a, b){ // sorting array by date (ascending)
+		    return a.date.getDate() - b.date.getDate();
+		});
+		console.log(allMessages);
+		cb(null,allMessages);
 	};
 
 	Message.getUnreadMessage = function(read, receiver, cb){
