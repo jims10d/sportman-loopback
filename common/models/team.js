9f48cf680119d048606f4eee985d08643886bfd9
Jimmy Chen <jimmy600d@gmail.com>
2018-05-10 18:17:35 +0700

    update function addTeam in model user

--- common/models/team.js
+++ common/models/team.js
@@ -241,7 +241,8 @@ module.exports = function(Team) {
 						//if this is the first Competition he see
 						console.log("tes");
 						theMembersNow = Member;
-						Team.updateAll({team_name: TeamName}, {team_coach: TeamCoach}, {team_squad: theMembersNow}, //update
+						//TODO fix updateAll function
+						Team.updateAll({team_name: TeamName}, {team_coach: TeamCoach, team_squad: theMembersNow}}, //update
 						function(err,info){
 							Team.findOne({where:{team_name: TeamName}},
 								function(err,instance){
@@ -260,7 +261,8 @@ module.exports = function(Team) {
 						}else{
 							//it's only the last Competition he's seen
 							theMembersNow = theMembersNow + ',' + Member;
-							Team.updateAll({team_name: TeamName}, {team_coach: TeamCoach}, {team_squad: theMembersNow}, //update
+							//TODO fix updateAll function
+							Team.updateAll({team_name: TeamName}, {team_coach: TeamCoach, team_squad: theMembersNow}}, //update
 							function(err,info){
 								Team.findOne({where:{team_name: TeamName}},
 									function(err,instance){
