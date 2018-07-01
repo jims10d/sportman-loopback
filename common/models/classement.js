44fa69f9a8fa60ce6f614eaa15c105bf0e9117d1
Jimmy Chen <jimmy600d@gmail.com>
2018-07-01 22:27:18 +0700

    add function getClassementByCompetitionId to model classement

--- common/models/classement.js
+++ common/models/classement.js
@@ -1,5 +1,25 @@
 module.exports = function(Classement) {
 
+    Classement.getClassementByCompetitionId = function(competitionId, cb){
+		Classement.find({where:{competition_id:competitionId}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
+    };
+    
+    Classement.remoteMethod(
+		'getClassementByCompetitionId',
+		{
+			accepts: {arg: 'competitionId', type: 'string'},
+			returns: {arg: 'classements', type: 'string', root: true},
+			http: {path: '/getClassementByCompetitionId', verb: 'get', source: 'query'},
+			description: "Get Classements instance by competition id"
+		}
+	);
 };
 
 
