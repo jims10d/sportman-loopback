c11fb9f7c645894e73f39e77d46f38fa0214cbf2
Jimmy <jimmychen10d@gmail.com>
2018-07-20 11:46:58 +0700

    create new property availableDayAndTime to model user

--- server/bin/automigrate.js
+++ server/bin/automigrate.js
@@ -97,109 +97,36 @@
 //   });
 // });
 
-// var app = require('../server');
-
-// var users = [ // kecils
-//   {
-//     id: 'Mqqfdfeergdfffdfgsdderjerikhfsdsdffsdffsdwe',
-//     username: 'referee12345',
-//     fullname: 'referee12345',
-//     password:'Referee12345',
-//     email: 'referee12345@gmail.com',
-//     role: 'Referee',
-//     photo: '',
-//     teamInvitation:'',
-//     team: '',
-//     teamRequested: '',
-//     ktp: '12391247192476942',
-//     age: '',
-//     address:'',
-//     position: '',
-//     hp: '',
-//     bio: '',
-//     profileCompleted:'',
-//     play: '',
-//     goal: '',
-//     assist: '',
-//     yellowCard: '',
-//     redCard:'',
-//     redCardGiven: '',
-//     yellowCardGiven: '',
-//     matchManaged: '',
-//     availableDay: '',
-//     availableTime: ''
-//   }
-// ];
- 
-// // this loads the accountDb configuration in ~/server/datasources.json
-// var dataSource = app.dataSources.SportmanDb;
- 
-// // this automigrates the Account model 
-// dataSource.autoupdate('user', function(err) { // kecil
-//   if (err) throw err;
- 
-//   // this loads the Account model from ~/common/models/Account.json
-//   var User = app.models.User; // besar
-//   var count = users.length; // kecils
-//   users.forEach(function(user) { // kecils kecil
-//      // insert new records into the Account table
-//      User.create(user, function(err, record) { // besar kecil
-//       if (err) return console.log(err);
- 
-//       console.log('Record created:', record);
- 
-//       count--;
- 
-//       if (count === 0) {
-//         console.log('done');
-//         dataSource.disconnect();
-//       }
-//     });
-//   });
-// });
-
 var app = require('../server');
 
-var matches = [ // kecils
+var users = [ // kecils
   {
-    match_name: 'QWE122',
-    match_venue: 'Planet Futsal',
-    match_time: '15:00',
-    match_countDown:'60',
-    match_length: '50',
-    match_break: '10',
-    match_date: '29/06/2018',
-    match_referee:'Mark Johnson',
-    match_homeTeam: 'Portugal',
-    match_awayTeam: 'Argentina',
-    match_homeTeamObj: '',
-    match_awayTeamObj: '',
-    match_fixture:'3',
-    match_number: '1',
-    match_started: 'true',
-    timer_status: 'false',
-    winner:'',
-    loser: '',
-    match_pair: '',
-    match_group: '',
-    match_status: '',
-    live_status: 'false',
-    timer:'',
-    countDownTimer: '',
-    countDownStarted: '',
-    halfTime: '',
-    fullTime: '',
-    referee_status: '',
-    goalHome: '',
-    goalAway:'',
-    assistHome: '',
-    assistAway: '',
-    yellowCardHome: '',
-    yellowCardAway: '',
-    redCardHome: '',
-    redCardAway: '',
-    substituteHome: '',
-    substituteAway: ''
+    id: 'Mqqqqqwqerjerikhfsdsdffsdffsdwe',
+    username: 'referee12345123',
+    fullname: 'referee12345123',
+    password:'Referee12345123',
+    email: 'referee12345123@gmail.com',
+    role: 'Referee',
+    photo: '',
+    teamInvitation:'',
+    team: '',
+    teamRequested: '',
+    ktp: '12391247192476942',
+    age: '',
+    address:'',
+    position: '',
+    hp: '',
+    bio: '',
+    profileCompleted:'',
+    play: '',
+    goal: '',
+    assist: '',
+    yellowCard: '',
+    redCard:'',
+    redCardGiven: '',
+    yellowCardGiven: '',
+    matchManaged: '',
+    availableDayAndTime: ''
   }
 ];
  
@@ -207,15 +134,15 @@ var matches = [ // kecils
 var dataSource = app.dataSources.SportmanDb;
  
 // this automigrates the Account model 
-dataSource.autoupdate('match', function(err) { // kecil
+dataSource.autoupdate('user', function(err) { // kecil
   if (err) throw err;
  
   // this loads the Account model from ~/common/models/Account.json
-  var Match = app.models.Match; // besar
-  var count = matches.length; // kecils
-  matches.forEach(function(match) { // kecils kecil
+  var User = app.models.User; // besar
+  var count = users.length; // kecils
+  users.forEach(function(user) { // kecils kecil
      // insert new records into the Account table
-     Match.create(match, function(err, record) { // besar kecil
+     User.create(user, function(err, record) { // besar kecil
       if (err) return console.log(err);
  
       console.log('Record created:', record);
@@ -229,6 +156,78 @@ dataSource.autoupdate('match', function(err) { // kecil
     });
   });
 });
+
+// var app = require('../server');
+
+// var matches = [ // kecils
+//   {
+//     match_name: 'QWE122',
+//     match_venue: 'Planet Futsal',
+//     match_time: '15:00',
+//     match_countDown:'60',
+//     match_length: '50',
+//     match_break: '10',
+//     match_date: '29/06/2018',
+//     match_referee:'Mark Johnson',
+//     match_homeTeam: 'Portugal',
+//     match_awayTeam: 'Argentina',
+//     match_homeTeamObj: '',
+//     match_awayTeamObj: '',
+//     match_fixture:'3',
+//     match_number: '1',
+//     match_started: 'true',
+//     timer_status: 'false',
+//     winner:'',
+//     loser: '',
+//     match_pair: '',
+//     match_group: '',
+//     match_status: '',
+//     live_status: 'false',
+//     timer:'',
+//     countDownTimer: '',
+//     countDownStarted: '',
+//     halfTime: '',
+//     fullTime: '',
+//     referee_status: '',
+//     goalHome: '',
+//     goalAway:'',
+//     assistHome: '',
+//     assistAway: '',
+//     yellowCardHome: '',
+//     yellowCardAway: '',
+//     redCardHome: '',
+//     redCardAway: '',
+//     substituteHome: '',
+//     substituteAway: ''
+//   }
+// ];
+ 
+// // this loads the accountDb configuration in ~/server/datasources.json
+// var dataSource = app.dataSources.SportmanDb;
+ 
+// // this automigrates the Account model 
+// dataSource.autoupdate('match', function(err) { // kecil
+//   if (err) throw err;
+ 
+//   // this loads the Account model from ~/common/models/Account.json
+//   var Match = app.models.Match; // besar
+//   var count = matches.length; // kecils
+//   matches.forEach(function(match) { // kecils kecil
+//      // insert new records into the Account table
+//      Match.create(match, function(err, record) { // besar kecil
+//       if (err) return console.log(err);
+ 
+//       console.log('Record created:', record);
+ 
+//       count--;
+ 
+//       if (count === 0) {
+//         console.log('done');
+//         dataSource.disconnect();
+//       }
+//     });
+//   });
+// });
 
 // var app = require('../server');
 
