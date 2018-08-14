5554de2caec09f82dddfbf304214286a132cbf9b
Jimmy <jimmychen10d@gmail.com>
2018-08-14 16:14:26 +0700

    update classement

--- common/models/classement.js
+++ common/models/classement.js
@@ -9,6 +9,17 @@ module.exports = function(Classement) {
 					cb(null,instance);
 				}
 			});
+	};
+	
+	Classement.getClassementByCompIdAndTeamName = function(compId, teamName, cb){
+		Classement.find({where:{competition_id:compId,team:teamName}},
+			function(err,instance){
+				if(instance===null){
+					cb(null,null);
+				}else{
+					cb(null,instance);
+				}
+			});
     };
     
     Classement.remoteMethod(
@@ -18,6 +29,19 @@ module.exports = function(Classement) {
 			returns: {arg: 'classements', type: 'string', root: true},
 			http: {path: '/getClassementByCompetitionId', verb: 'get', source: 'query'},
 			description: "Get Classements instance by competition id"
+		}
+	);
+
+	Classement.remoteMethod(
+		'getClassementByCompIdAndTeamName',
+		{
+			accepts: [
+				{arg: 'compId', type: 'string'},
+				{arg: 'teamName', type: 'string'}
+			],
+			returns: {arg: 'classement', type: 'string', root: true},
+			http: {path: '/getClassementByCompIdAndTeamName', verb: 'get', source: 'query'},
+			description: "Get Classement instance by competition id and team name"
 		}
 	);
 };
